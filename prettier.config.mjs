b7fdd8c36610cb5e38ee5cd2edf52eb029a30ad2
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-09-14 10:12:54 +0000

    fix: Update Prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,11 +3,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: 'es5',
     tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    jsxSingleQuote: true,
 };
 
 export default config;
