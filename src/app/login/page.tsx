b86986398779601e6a7c7741507e2c2fc35e2610
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-09-18 12:26:58 +0000

    fix: `next/router` から `next/navigation` を使用するように変更
    * App Router では `next/router` を使用できない。

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
     const router = useRouter();
