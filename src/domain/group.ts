91817ee02f3ed327eb885316025df8f3b97d9e24
Sakura584 <sakura412629@gmail.com>
2025-10-01 13:33:29 +0900

    domain/group.tsとinfra/group/group-converter.ts,group-repo.tsの作成 * groupのドメイン層とインフラ層の作成

---
+++ src/domain/group.ts
@@ -0,0 +1,17 @@
+interface Group{
+    id: string;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface GroupRepository {
+    findById(groupId: string): Promise<Group | null>;
+    create(group: Omit<Group, 'id' | 'createdAt' | 'updatedAt'>, userId: string): Promise<Group>;
+    update(group: Partial<Group>): Promise<Group>;
+    delete(groupId: string): Promise<void>;
+    findAllByUserId(userId: string): Promise<Group[]>;
+    addUserToGroup(groupId: string, userId: string): Promise<void>;
+}
+
+export type{ Group,GroupRepository };
