c8da30a521324e5eddc28759a7500a1a0b1ce22a
Harineko0 <harineko0927@gmail.com>
2025-10-04 14:55:21 +0900

    Add lib/date.ts

---
+++ src/lib/date.ts
@@ -0,0 +1,9 @@
+import type {FieldValue, WithFieldValue} from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
+
+/**
+ * DateをTimestampに変換する
+ * @param date Date, FieldValue, Timestamp, WithFieldValue<Date>
+ */
+export const toTimestamp = (date: Date | Timestamp | FieldValue | WithFieldValue<Date>) =>
+    date instanceof Date ? Timestamp.fromDate(date) : date;
