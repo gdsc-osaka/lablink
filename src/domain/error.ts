b300469b9997d866f1fd2d7f2d236e2e9f14ceb3
Harineko0 <harineko0927@gmail.com>
2025-10-04 14:55:14 +0900

    Add domain/error.ts, infra/error.ts

---
+++ src/domain/error.ts
@@ -0,0 +1,16 @@
+import {errorBuilder, InferError} from "obj-err";
+
+export const NotFoundError = errorBuilder("NotFoundError");
+type NotFoundError = InferError<typeof NotFoundError>;
+
+export const PermissionDeniedError = errorBuilder("PermissionDeniedError");
+type PermissionDeniedError = InferError<typeof PermissionDeniedError>;
+
+export const UnauthenticatedError = errorBuilder("UnauthenticatedError");
+type UnauthenticatedError = InferError<typeof UnauthenticatedError>;
+
+// その他のエラー
+export const UnknownError = errorBuilder("UnknownError");
+type UnknownError = InferError<typeof UnknownError>;
+
+export type DBError = NotFoundError | PermissionDeniedError | UnauthenticatedError | UnknownError;
