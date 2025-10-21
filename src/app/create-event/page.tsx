d8f8eca6e466e99ef64d0367d7a85b526134c6c9
Nagomu0128 <kyoshidk@gmail.com>
2025-10-21 21:08:17 +0900

    fix: html error

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+﻿"use client";
 
 import { useState } from "react";
 import Link from "next/link";
@@ -158,72 +158,8 @@ const CreateEventPage = () => {
                     </div>
                 </form>
             </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="details"
-              className="block text-sm font-medium text-black mb-1"
-            >
-              イベントの詳細
-            </label>
-            <textarea
-              id="details"
-              name="description"
-              rows={4}
-              value={eventData.description}
-              placeholder="新しく研究室配属された学部4年の学生の歓迎会としてたこ焼きパーティーをする外部進学した留学生のためにたこ焼きパーティーをする"
-              onChange={handleChange}
-              className="mt-2 block w-full p-3 bg-white border border-gray-400 rounded-lg focus:outline-none focus:border-gray-400 text-black"
-            ></textarea>
-          </div>
-
-                    <div className="flex justify-end">
-                        <Link href="/ai-suggest">
-                            <button
-                                type="button"
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            >
-                                AIのsuggestへ
-                            </button>
-                        </Link>
-                    </div>
-                </form>
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="details"
-              className="block text-sm font-medium text-black mb-1"
-            >
-              イベントの詳細を記入してください
-            </label>
-            <textarea
-              id="details"
-              name="details"
-              rows={4}
-              value={eventData.description}
-              placeholder="新しく研究室配属された学部4年の学生の歓迎会としてたこ焼きパーティーをする外部進学した留学生のためにたこ焼きパーティーをする"
-              onChange={handleChange}
-              className="mt-2 block w-full p-3 bg-white border border-gray-400 rounded-lg focus:outline-none focus:border-gray-400 text-black"
-            ></textarea>
-          </div>
-
-          <div className="flex justify-end">
-            <Link href="/ai-suggest">
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                AIのsuggestへ
-              </button>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </main>
-  );
+        </main>
+    );
 };
 
 export default CreateEventPage;
