e8a6488b9a3de339e975bad34f6eea305e47bdd3
Nagomu0128 <kyoshidk@gmail.com>
2025-10-22 16:05:28 +0900

    add shdcn

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+// shadcn用utility関数
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
