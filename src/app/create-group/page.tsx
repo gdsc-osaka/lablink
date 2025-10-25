af692df513a8c5a10cf3c55bf6ccb0ea3140a093
Nagomu0128 <kyoshidk@gmail.com>
2025-10-25 18:51:01 +0900

    shadcnに置換

--- src/app/create-group/page.tsx
+++ src/app/create-group/page.tsx
@@ -4,13 +4,14 @@ import { FormEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const CreateGroupPage = () => {
     const [groupName, setGroupName] = useState("");
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
+        // TODO:Toasterの使い方が分からず断念
         console.log("グループが作成されました:", { groupName });
     };
 
@@ -23,13 +24,13 @@ const CreateGroupPage = () => {
                 <h1 className="mb-2 text-xl font-semibold text-slate-900">
                     {"グループを作成する"}
                 </h1>
-                <p className="mb-3 text-sm text-slate-600">
-                    {"グループ名"}
-                </p>
+                <Label className="mb-3 text-sm text-slate-600">
+                    "グループ名"
+                </Label>
                 <Input
                     value={groupName}
                     onChange={(event) => setGroupName(event.target.value)}
-                    placeholder="グループ名を入力"
+                    placeholder="グループ名を入力してください"
                     className="mb-6 h-10 border border-slate-300 bg-white text-base shadow-inner focus-visible:ring-blue-500"
                     required
                 />
