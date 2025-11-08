567e35fbd2e18e12855c9b31950660861a2e0c24
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-11-08 11:18:51 +0900

    feat: 標準フォントに Google Sans を使用
    Google Sans には日本語が収録されていないので、フォールバックとして Noto Sans JP も適用済み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+import { googleSans, notoSansJP } from "@/lib/font";
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -23,13 +13,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" style={{ height: "100%" }}>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                style={{ height: "100%" }}
-            >
-                {children}
-            </body>
+        <html
+            lang="en"
+            style={{ height: "100%" }}
+            className={`${googleSans.variable} ${notoSansJP.variable} antialiased`}
+        >
+            <body style={{ height: "100%" }}>{children}</body>
         </html>
     );
 }
