bfb3c1377ced7d3843cda7a68971a60d3b3d9b9b
Nagomu0128 <kyoshidk@gmail.com>
2025-11-10 19:49:02 +0900

    fix: utils import error

--- src/infra/utils.ts
+++ src/infra/utils.ts
@@ -1,4 +1,4 @@
-import { Timestamp } from "firebase-admin/firestore";
+import { Timestamp } from "firebase/firestore";
 
 export const toFirestoreTimestamp = (value: Date | Timestamp): Timestamp =>
     value instanceof Timestamp ? value : Timestamp.fromDate(value);
