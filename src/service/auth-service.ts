c5f44f45cf25e78a4e237399c5e6901b82f2b73a
Harineko0 <harineko0927@gmail.com>
2025-11-26 21:58:13 +0900

    Add service/auth-service.ts

---
+++ src/service/auth-service.ts
@@ -0,0 +1,19 @@
+import { ResultAsync } from "neverthrow";
+import { DBError } from "@/domain/error";
+import { createNewUser, User, UserRepository } from "@/domain/user";
+import { AuthError, AuthRepository } from "@/domain/auth";
+
+export interface AuthService {
+    signInWithGoogle(): ResultAsync<User, DBError | AuthError>;
+}
+
+export const createAuthService = (
+    userRepository: UserRepository,
+    authRepository: AuthRepository,
+): AuthService => ({
+    signInWithGoogle: () =>
+        authRepository
+            .signInWithGoogle()
+            .map(createNewUser)
+            .andThen(userRepository.create),
+});
