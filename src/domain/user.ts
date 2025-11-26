19abf28bed89774181ba8c58a68cbe76139de8dc
Harineko0 <harineko0927@gmail.com>
2025-11-26 21:58:27 +0900

    Add createNewUser on domain/user.ts

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,6 +1,7 @@
 import { Timestamp } from "firebase/firestore";
 import { ResultAsync } from "neverthrow";
 import { DBError } from "@/domain/error";
+import { User as AuthUser } from "firebase/auth";
 
 export interface User {
     email: string;
@@ -12,4 +13,13 @@ export interface UserRepository {
     create(user: User): ResultAsync<User, DBError>;
     findById(uid: string): ResultAsync<User, DBError>;
     update(user: User): ResultAsync<User, DBError>;
+}
+
+export const createNewUser = (user: AuthUser): User => {
+    const timestamp = Timestamp.now();
+    return {
+        email: user.email!,
+        created_at: timestamp,
+        updated_at: timestamp,
+    };
 }
