920374ce7aad47cb3879c9e5b195e89cb34016e6
litchi7777 <litchi7777@gmail.com>
2025-12-10 16:50:13 +0900

    refactor: unify to authprovider

--- src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthProvider } from "@/provider/AuthProvider";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <AuthProvider>{children}</AuthProvider>;
-}
