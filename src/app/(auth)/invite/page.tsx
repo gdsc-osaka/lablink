2aa07223a7d51f2b5a1e5053d2690b6d8d7f360c
Sakura584 <sakura412629@gmail.com>
2026-02-11 01:12:22 +0900

    invitation-serviceの変更に伴い、変数を削除

--- src/app/(auth)/invite/page.tsx
+++ src/app/(auth)/invite/page.tsx
@@ -23,7 +23,6 @@ function InvitePageContent() {
             createInvitationService(
                 invitationRepo,
                 firestoreGroupRepository,
-                firestoreUserGroupRepository,
             ),
         [],
     );
